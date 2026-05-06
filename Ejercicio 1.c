419379dbe4b7af9709f9ba31373c0c3dc7e88a9b
Emmanuel-333 <juarezobregonemmanuel@gmail.com>
2026-05-05 19:06:46 -0600

    Create Ejercicio 1.c

---
+++ Ejercicio 1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+{
+  int a, b;
+  printf("Ingresa el primer nmero: ");
+  scanf("%d", &a);
+  printf("Ingresa el segundo numero: ");
+  scanf("%d", &b);
+
+  printf("Suma= %d\n", a + b );
+  printf("resta= %d\n", a - b);
+  printf("multiplicacion= %d\n", a*b );
+  return 0;
+}
