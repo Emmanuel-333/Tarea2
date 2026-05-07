08dc90e40cb8cd3b36a30dce6dd2bf1ce8cbc471
Emmanuel-333 <juarezobregonemmanuel@gmail.com>
2026-05-07 17:46:28 -0600

    Create Programa11.c

---
+++ Programa11.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main ()
+{
+  int suma=0;
+  int i;
+  for(i=10; i<=50;i++)
+    {
+      suma= suma + i;
+    }
+printf("el resultado de la suma es: %d", suma);
+return 0;
+}
